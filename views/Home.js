a8798cfad5d25d4dd1624d064dc301ec08cab162
Pavel Chernov <pvchernoff@gmail.com>
2022-10-06 13:48:44 +0300

    feat: add floating ADD TASK button in Home.js.

--- views/Home.js
+++ views/Home.js
@@ -1,7 +1,8 @@
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Header from '../components/Header';
 import List from '../components/List';
 import PropTypes from 'prop-types';
+import AddButton from '../assets/add-btn.svg';
 // import {MainContext} from '../contexts/MainContext';
 // import {useContext, useLayoutEffect} from 'react';
 
@@ -15,18 +16,28 @@ const Home = ({navigation}) => {
   // });
 
   return (
-    <SafeAreaView style={styles.container}>
+    <>
       <Header navigation={navigation} />
-      <List navigation={navigation} />
-    </SafeAreaView>
+      <View>
+        <List navigation={navigation} />
+        <AddButton
+          style={styles.addBtn}
+          height={'16%'}
+          width={'16%'}
+          onPress={() => {
+            navigation.navigate('Upload');
+          }}
+        />
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'rgba(33,32,40,255)',
+  addBtn: {
+    position: 'absolute',
+    bottom: 80,
+    left: '42%',
   },
 });
 
