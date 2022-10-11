c2c3ff70221e27629f469ad4561c63ef0e0814f1
eliasleipola <Elias.Leipola@metropolia.fi>
2022-10-11 14:00:00 +0300

    removed unnecessary button, styed text

--- views/Profile.js
+++ views/Profile.js
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import PropTypes from 'prop-types';
 import {useTag} from '../hooks/ApiHooks';
 import {mediaUrl} from '../utils/variables';
-import {Avatar, Button, Card, Icon, ListItem, Text} from '@rneui/themed';
+import {Button, Card, ListItem, Text} from '@rneui/themed';
 import {ScrollView, ActivityIndicator} from 'react-native';
 
 const Profile = ({navigation}) => {
@@ -38,50 +38,55 @@ const Profile = ({navigation}) => {
 
   return (
     <ScrollView>
-      <Card>
-        <Card.Title>
-          <Icon name="person" />
-          <Text style={styles.textStyle}>{user.username}</Text>
+      <Card
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Card.Title style={{fontWeight: '900', fontSize: 28}}>
+          Account details
         </Card.Title>
         <Card.Divider />
 
         <Card.Image
-          style={{padding: 0, borderRadius: 20}}
+          style={{
+            padding: 0,
+            borderRadius: 100,
+            resizeMode: 'contain',
+            alignSelf: 'center',
+            width: 200,
+          }}
           source={{
             uri: avatar,
           }}
           PlaceholderContent={<ActivityIndicator />}
         />
         <ListItem>
-          <Avatar
-            avatarStyle={{borderRadius: 20}}
-            icon={{
-              name: 'contact-mail',
-              type: 'material',
-              color: 'black',
-            }}
-            containerStyle={{backgroundColor: '#aaa'}}
-          />
-          <Text style={styles.textStyle}>{user.email}</Text>
+          <Text style={styles.textStyle}>Username: </Text>
+          <Text style={{color: 'blue', textAlign: 'center'}}>
+            {user.username}
+          </Text>
         </ListItem>
         <ListItem>
-          <Text style={styles.textStyle}>Full name: {user.full_name}</Text>
+          <Text style={styles.textStyle}>User ID: </Text>
+          <Text style={{color: 'blue', textAlign: 'center'}}>
+            {user.user_id}
+          </Text>
         </ListItem>
         <ListItem>
-          <Text style={styles.textStyle}>userId: {user.user_id}</Text>
+          <Text style={styles.textStyle}>Email: </Text>
+          <Text style={{color: 'blue', textAlign: 'center'}}>{user.email}</Text>
         </ListItem>
-        <Button
-          buttonStyle={{
-            borderRadius: 20,
-            marginTop: 5,
-            marginBottom: 5,
-          }}
-          title={'MyFiles'}
-          type="clear"
-          onPress={() => {
-            navigation.navigate('MyTasks');
-          }}
-        />
+        <ListItem>
+          <Text style={styles.textStyle}>Full name: </Text>
+          <Text style={{color: 'blue', textAlign: 'center'}}>
+            {user.full_name}
+          </Text>
+        </ListItem>
+
         <Button
           buttonStyle={{
             borderRadius: 20,
@@ -100,7 +105,6 @@ const Profile = ({navigation}) => {
             marginBottom: 5,
           }}
           title={'Logout'}
-          type="clear"
           onPress={logout}
         />
       </Card>
@@ -125,7 +129,7 @@ const styles = StyleSheet.create({
     color: 'black',
   },
   textStyle: {
-    color: 'blue',
+    color: 'black',
     fontWeight: 'bold',
   },
 });
