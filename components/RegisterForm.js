37424c06bdda31588702ff9f16dc3c99a668a485
Pavel Chernov <pvchernoff@gmail.com>
2022-10-11 12:52:22 +0300

    feat: add Terms and Policy Alerts into Login. refactor: style LoginForm and RegisterForm.

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, ScrollView, StyleSheet, Alert} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import {useUser} from '../hooks/ApiHooks';
 import {Input, Button, Card} from '@rneui/themed';
+import {colorSchema} from '../utils/variables';
+import PropTypes from 'prop-types';
 
 export const RegisterForm = () => {
   const {checkUsername, postUser} = useUser();
@@ -38,128 +40,210 @@ export const RegisterForm = () => {
     }
   };
 
+  const openTerms = () => {
+    Alert.alert('CleanMe! Terms', 'Terms to be added', [
+      {
+        text: 'OK',
+        onPress: () => {},
+      },
+    ]);
+  };
+
+  const openPolicy = () => {
+    Alert.alert('CleanMe! Privacy Policy', 'Policy to be added', [
+      {
+        text: 'OK',
+        onPress: () => {},
+      },
+    ]);
+  };
+
   return (
-    <Card>
-      <Card.Title>Registration form</Card.Title>
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-          minLength: 3,
-          validate: async (value) => {
-            return await checkUsername(value);
-          },
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            placeholder="username"
-          />
+    <>
+      <ScrollView style={styles.container}>
+        <Card.Title style={{fontSize: 26, color: colorSchema.mainColor}}>
+          Registration
+        </Card.Title>
+        <Controller
+          control={control}
+          rules={{
+            required: {value: true, message: 'This is required'},
+            minLength: {
+              value: 3,
+              message: 'Username must be at least 3 characters',
+            },
+            validate: async (value) => {
+              return await checkUsername(value);
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="username *"
+            />
+          )}
+          name="username"
+        />
+        {/* {errors.username?.type === 'required' && <Text>This is required.</Text>}
+        {errors.username?.type === 'minLength' && <Text>Minimum 3 chars!</Text>} */}
+        {errors.username?.type === 'validate' && (
+          <Text style={styles.checkUsername}>Username is already taken</Text>
         )}
-        name="username"
-      />
-      {errors.username?.type === 'required' && <Text>This is required.</Text>}
-      {errors.username?.type === 'minLength' && <Text>Minimum 3 chars!</Text>}
-      {errors.username?.type === 'validate' && (
-        <Text>Username is already taken</Text>
-        // <Text>{errors.username.message}</Text>
-      )}
 
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-          minLength: 5,
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            secureTextEntry={true}
-            placeholder="password"
-          />
-        )}
-        name="password"
-      />
-      {errors.password?.type === 'minLength' && <Text>Minimum 5 chars!</Text>}
-      {errors.password?.type === 'required' && <Text>This is required.</Text>}
+        <Controller
+          control={control}
+          rules={{
+            required: {value: true, message: 'This is required'},
+            minLength: {
+              value: 5,
+              message: 'Password must be at least 5 characters.',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              secureTextEntry={true}
+              placeholder="password *"
+              errorMessage={
+                errors.password && <Text>{errors.password.message}</Text>
+              }
+            />
+          )}
+          name="password"
+        />
+        {/* {errors.password?.type === 'minLength' && <Text>Minimum 5 chars!</Text>}
+        {errors.password?.type === 'required' && <Text>This is required.</Text>} */}
 
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-          minLength: 5,
-          validate: (value) => {
-            if (value === getValues('password')) {
-              return true;
-            } else {
-              return 'passwords are not the same';
-            }
-          },
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            secureTextEntry={true}
-            placeholder="confirm password"
-            errorMessage={
-              errors.passwordConfirm && (
-                <Text>{errors.passwordConfirm.message}</Text>
-              )
-            }
-          />
-        )}
-        name="passwordConfirm"
-      />
-      {errors.password?.type === 'minLength' && <Text>Minimum 5 chars!</Text>}
-      {errors.password?.type === 'required' && <Text>This is required.</Text>}
+        <Controller
+          control={control}
+          rules={{
+            required: {value: true, message: 'This is required'},
+            minLength: {
+              value: 5,
+              message: 'Confirm password must be at least 5 characters.',
+            },
+            validate: (value) => {
+              if (value === getValues('password')) {
+                return true;
+              } else {
+                return 'Passwords are not the same';
+              }
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              secureTextEntry={true}
+              placeholder="confirm password *"
+              errorMessage={
+                errors.passwordConfirm && (
+                  <Text>{errors.passwordConfirm.message}</Text>
+                )
+              }
+            />
+          )}
+          name="passwordConfirm"
+        />
+        {/* {errors.password?.type === 'minLength' && <Text>Minimum 5 chars!</Text>}
+        {errors.password?.type === 'required' && <Text>This is required.</Text>} */}
 
-      <Controller
-        control={control}
-        rules={{
-          required: {value: true, message: 'this is required'},
-          pattern: {
-            value: /^[a-z0-9.]{1,20}@[a-z0-9.]{2,20}[.][a-z0-9.]{2,10}/i,
-            message: 'must be a valid email!',
-          },
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            secureTextEntry={true}
-            placeholder="email"
-            keyboardType="email-address"
-            errorMessage={errors.email && <Text>{errors.email.message}</Text>}
-          />
-        )}
-        name="email"
-      />
-      {/* {errors.email?.type === 'required' && <Text>This field is required</Text>}
+        <Controller
+          control={control}
+          rules={{
+            required: {value: true, message: 'this is required'},
+            pattern: {
+              value: /^[a-z0-9.]{1,20}@[a-z0-9.]{2,20}[.][a-z0-9.]{2,10}/i,
+              message: 'must be a valid email!',
+            },
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="email *"
+              keyboardType="email-address"
+              errorMessage={errors.email && <Text>{errors.email.message}</Text>}
+            />
+          )}
+          name="email"
+        />
+        {/* {errors.email?.type === 'required' && <Text>This field is required</Text>}
       {errors.email?.type === 'keyboardType' && (
         <Text>This should be an email!</Text>
       )} */}
 
-      <Controller
-        control={control}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            placeholder="full name"
-          />
-        )}
-        name="full_name"
-      />
+        <Controller
+          control={control}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              placeholder="full name"
+            />
+          )}
+          name="full_name"
+        />
 
-      <Button title="Sign up" onPress={handleSubmit(register)} />
-    </Card>
+        <Button
+          buttonStyle={styles.btn}
+          title="Register!"
+          onPress={handleSubmit(register)}
+        />
+        <Text>
+          By signing up on this application, you’re also agreeing to our&#x2007;
+          <Text
+            onPress={openTerms}
+            style={{color: colorSchema.mainColor, fontWeight: 'bold'}}
+          >
+            Terms of Service
+          </Text>{' '}
+          and&#x2007;
+          <Text
+            onPress={openPolicy}
+            style={{color: colorSchema.mainColor, fontWeight: 'bold'}}
+          >
+            Privacy Policy
+          </Text>
+        </Text>
+      </ScrollView>
+    </>
   );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  dateInput: {
+    fontSize: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+  },
+  deadlineWrap: {
+    flexDirection: 'row',
+  },
+  checkUsername: {
+    color: colorSchema.red,
+    marginLeft: 10,
+    marginTop: -20,
+  },
+  btn: {
+    marginBottom: 20,
+    backgroundColor: colorSchema.mainColor,
+    borderRadius: 40,
+  },
+});
+
+RegisterForm.propTypes = {
+  navigation: PropTypes.object,
 };
--- views/Login.js
+++ views/Login.js
@@ -1,12 +1,21 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Keyboard, TouchableOpacity, View, Text} from 'react-native';
+import {
+  StyleSheet,
+  Keyboard,
+  TouchableOpacity,
+  View,
+  Text,
+  ScrollView,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import {useLogin} from '../hooks/ApiHooks';
 import {LoginForm} from '../components/LoginForm';
 import {RegisterForm} from '../components/RegisterForm';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Header from '../components/Header';
+import {colorSchema} from '../utils/variables';
 
-const Login = () => {
+const Login = ({navigation}) => {
   const {checkToken} = useLogin();
   const [showRegForm, setShowRegForm] = useState(false);
 
@@ -15,42 +24,45 @@ const Login = () => {
   });
 
   return (
-    <SafeAreaView
-      style={{flex: 1, justifyContent: 'space-between', alignItems: 'stretch'}}
-    >
-      <TouchableOpacity
-        onPress={Keyboard.dismiss}
-        style={{flex: 1}}
-        activeOpacity={1}
-      >
-        <View>
+    <>
+      <Header navigation={navigation} />
+      <ScrollView style={styles.container}>
+        {showRegForm ? <RegisterForm /> : <LoginForm />}
+        <Text
+          onPress={() => {
+            setShowRegForm(!showRegForm);
+          }}
+          style={{marginTop: 20, textAlign: 'center'}}
+        >
           {showRegForm ? (
-            <RegisterForm style={styles.inner} />
+            <>
+              <Text>Already registered? </Text>
+              <Text style={{color: colorSchema.mainColor, fontWeight: 'bold'}}>
+                Log in
+              </Text>
+            </>
           ) : (
-            <LoginForm style={styles.inner} />
+            <>
+              <Text>No account yet? </Text>
+              <Text style={{color: colorSchema.mainColor, fontWeight: 'bold'}}>
+                Register
+              </Text>
+            </>
           )}
-          <Text
-            onPress={() => {
-              setShowRegForm(!showRegForm);
-            }}
-            style={{marginTop: 20, textAlign: 'center'}}
-          >
-            {showRegForm
-              ? 'Already registered? Log in'
-              : 'No account yet? Register a new account'}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    </SafeAreaView>
+        </Text>
+      </ScrollView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colorSchema.bgrColor,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
   FlexGrowOne: {
     flexGrow: 1,
-  },
-  container: {
-    padding: 16,
   },
   animation: {
     justifyContent: 'center',
@@ -66,71 +78,3 @@ Login.propTypes = {
 };
 
 export default Login;
-
-/* const checkToken = async () => {
-    try {
-      const userToken = await AsyncStorage.getItem('userToken');
-      console.log('token: ', userToken);
-      if (userToken != null) {
-        const userData = await getUserByToken(userToken);
-        console.log('user data: ', userData);
-        setIsLoggedIn(true);
-        setUser(userData);
-      }
-    } catch (error) {
-      console.log('check token failed', error);
-    }
-  };
-
-  useEffect(() => {
-    checkToken();
-  }, []);
-
-  return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.container}
-    >
-      <TouchableOpacity
-        onPress={Keyboard.dismiss}
-        style={{flex: 1}}
-        activeOpacity={1}
-      >
-        <View>
-          {showRegForm ? (
-            <RegisterForm style={styles.inner} />
-          ) : (
-            <LoginForm style={styles.inner} />
-          )}
-          <Button
-            title={
-              showRegForm
-                ? 'Already registered? Log in'
-                : 'No account yet? Register a new account'
-            }
-            onPress={() => {
-              setShowRegForm(!showRegForm);
-            }}
-            style={{marginTop: 20}}
-          ></Button>
-        </View>
-      </TouchableOpacity>
-    </KeyboardAvoidingView>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: -24,
-  },
-  inner: {
-    // marginBottom: 15,
-  },
-});
-
-Login.propTypes = {
-  navigation: PropTypes.object,
-};
-
-export default Login; */
