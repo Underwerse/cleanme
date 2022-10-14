7594906b0f10f2a5adf6b679f8bcc5a34ca4fb60
Pavel Chernov <pvchernoff@gmail.com>
2022-10-14 14:19:15 +0300

    refactor: change MyTasks and MyFavorites style

--- views/MyFavorites.js
+++ views/MyFavorites.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
 import List from '../components/List';
-import Header from '../components/Header';
+import {StyleSheet, View} from 'react-native';
+import {Card} from '@rneui/themed';
+import {colorSchema} from '../utils/variables';
 
 const MyFavorites = ({navigation}) => {
   return (
     <>
-      <Header navigation={navigation} />
+      <View style={styles.container}>
+        <Card.Title
+          style={{
+            fontWeight: '900',
+            fontSize: 30,
+            color: colorSchema.primaryTextColor,
+            paddingTop: 20,
+          }}
+        >
+          My Favorites
+        </Card.Title>
+      </View>
       <List
         navigation={navigation}
         myFilesOnly={false}
@@ -15,6 +28,14 @@ const MyFavorites = ({navigation}) => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colorSchema.bgrColor,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+});
 
 MyFavorites.propTypes = {
   navigation: PropTypes.object,
--- views/MyTasks.js
+++ views/MyTasks.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
 import List from '../components/List';
-import Header from '../components/Header';
+import {Card} from '@rneui/themed';
+import {colorSchema} from '../utils/variables';
+import {StyleSheet, View} from 'react-native';
 
 const MyTasks = ({navigation}) => {
   return (
     <>
-      <Header navigation={navigation} />
+      <View style={styles.container}>
+        <Card.Title
+          style={{
+            fontWeight: '900',
+            fontSize: 30,
+            color: colorSchema.primaryTextColor,
+            paddingTop: 20,
+          }}
+        >
+          My Tasks
+        </Card.Title>
+      </View>
       <List
         navigation={navigation}
         myFilesOnly={true}
@@ -15,6 +28,14 @@ const MyTasks = ({navigation}) => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colorSchema.bgrColor,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+});
 
 MyTasks.propTypes = {
   navigation: PropTypes.object,
